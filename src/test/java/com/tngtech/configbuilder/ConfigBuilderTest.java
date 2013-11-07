2246de10bdd7f5296de9394a9b2023bafe297120
Fabian Winter <fabian.winter@tngtech.com>
2013-11-07 13:53:41 +0100

    Of course, some fixes were necessary

--- src/test/java/com/tngtech/configbuilder/ConfigBuilderTest.java
+++ src/test/java/com/tngtech/configbuilder/ConfigBuilderTest.java
@@ -25,6 +25,7 @@ import java.util.Properties;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
