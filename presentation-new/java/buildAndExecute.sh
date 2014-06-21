b21e8995925258d10c48717cadffefacc69b3869
Thomas Endres <Thomas-Endres@gmx.de>
2014-06-21 12:02:13 +0200

    Integrated error output for file execution

--- presentation-new/java/buildAndExecute.sh
+++ presentation-new/java/buildAndExecute.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd ${DIR}
 
-mvn compile exec:java -q -Dexec.mainClass=${CONFIG_CLASS} | tee build.log  | grep -v '^\['
+mvn compile exec:java -e -q -Dexec.mainClass=${CONFIG_CLASS} | tee build.log  | grep -v '^\['
 
 if [ $? -ne 0 ]
 then
