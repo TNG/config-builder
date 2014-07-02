7da06f43a5de083f28e1f42599030c608ad7e952
Matthias Bollwein <matthias.bollwein@gmail.com>
2014-07-02 21:31:37 +0200

    added classes to editor.html

--- presentation-new/java/src/main/java/com/tngtech/test/propertyvalue/Config.java
+++ presentation-new/java/src/main/java/com/tngtech/test/propertyvalue/Config.java
@@ -2,12 +2,14 @@
 package com.tngtech.test.propertyvalue;
 
 import com.tngtech.configbuilder.ConfigBuilder;
+import com.tngtech.configbuilder.annotation.propertyloaderconfiguration.PropertiesFiles;
 import com.tngtech.configbuilder.annotation.valueextractor.CommandLineValue;
 import com.tngtech.configbuilder.annotation.valueextractor.DefaultValue;
 import com.tngtech.configbuilder.annotation.valueextractor.PropertyValue;
 import com.tngtech.test.common.JSONHelper;
 
 // shown
+@PropertiesFiles("config")
 public class Config {
     // not shown
     public static void main(String[] args) throws Exception {
